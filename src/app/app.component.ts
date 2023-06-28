618b979f5cc46047c078dc5093d3ddfa7ea90f9f
Sean B <strawhorse@gmail.com>
2023-06-28 20:04:46 +0100

    Changes to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'General', url: '/general', icon: 'globe' },
+    { title: 'Economy/Business', url: '/business', icon: 'business' },
+    { title: 'Sport', url: '/sport', icon: 'football' },
+    { title: 'Entertainment/Celebrity', url: '/entertainment', icon: 'radio' },
+    { title: 'Tech', url: '/technology', icon: 'phone-portrait' },
+    { title: 'Health', url: '/health', icon: 'heart' },
+    { title: 'Science', url: '/science', icon: 'telescope' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  
   constructor() {}
 }
